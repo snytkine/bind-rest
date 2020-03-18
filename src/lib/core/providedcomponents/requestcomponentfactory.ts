ea3d473b8054a0eba4a2c4b61c2da225b9d554f7
Dmitri <dmitri.snytkine@adp.com>
2020-03-18 08:36:06 -0400

    Updated Context to make it implement IScopedComponentStorage

---
+++ src/lib/core/providedcomponents/requestcomponentfactory.ts
@@ -0,0 +1,10 @@
+import {
+  ComponentScope,
+  getComponentMeta,
+  IfIocComponent,
+  IfIocContainer,
+  IocComponentGetter,
+  IScopedComponentStorage,
+} from 'bind';
+import { Context } from '../context';
+

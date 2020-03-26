cd1487011cc67e1eef0097bfa9233b47b52838d1
Dmitri <dmitri.snytkine@adp.com>
2020-03-25 20:31:18 -0400

    Using http-method-enum instead of RequestMethod enum for all request methods

--- src/lib/core/application.ts
+++ src/lib/core/application.ts
@@ -8,24 +8,16 @@ import {
   AppErrorHandlerFunc,
   AppErrorHandler,
 } from '../interfaces';
-import {MiddlewareFunc} from '../types'
-import {
-  SYM_MIDDLEWARE_PRIORITY,
-  SYM_MIDDLEWARE_NAME
-} from '../decorators/metaprops';
+import { MiddlewareFunc } from '../types';
 
-import {
-  ComponentType,
-  RequestMethodEnum as RequestMethod
-} from '../enums';
+
 import {
   errorHandler,
   responseWriter,
   rejectLater,
   registerProcessEventListeners,
-  getRouter,
-  RouterErrorHandler
-} from './apputils'
+  RouterErrorHandler,
+} from './apputils';
 
 import {
   IfIocContainer,
@@ -35,13 +27,14 @@ import {
 
 import {
   IRouter,
-  IRouterConstructor
+  IRouterConstructor,
 } from '../interfaces/irouter';
 import {
   AllRoutes,
-  SYM_ALL_ROUTES
-} from './apputils/allroutes'
-import * as path from "path";
+  SYM_ALL_ROUTES,
+} from './apputils/allroutes';
+import * as path from 'path';
+import setupRoutes from './apputils/setuproutes';
 
 const debug = require('debug')('promiseoft:runtime:application');
 
@@ -107,13 +100,13 @@ export class Application {
      * All components are loaded into container
      * Now parse all controllers and add routes to router
      */
-
+    setupRoutes(this.container);
 
     /**
      * Get all Middleware, sort then by order (lower order first)
      * and create array of middleware functions.
      */
-
+    //const aMW: Array <MiddlewareFunc> = setupMiddlewares(this.container);
 
     //debug(`${TAG} starting application with baseDir: ${options.baseDir} with timeout ${this.timeout} milliseconds`);
 
@@ -131,58 +124,58 @@ export class Application {
 
     //const loadedComponents = loadComponents(options.baseDir);
     //debug(`LOADED FILES **** ${loadedComponents.length}   ${JSON.stringify(loadedComponents)}`);
-   // debug(`Registered Components: \n${Container.componentsList}`);
+    // debug(`Registered Components: \n${Container.componentsList}`);
 
     //const allComponents = loadedComponents.concat(extraComponents);
 
-/*
+    /*
 
-    const aMW: Array<MiddlewareFunc> = allComponents
-    .filter(_ => _.type === ComponentType.MIDDLEWARE)
-    .map(_ => <MiddlewareFunc>_.payload)
-    .reduce((prev, curr) => prev.concat(curr), []);
+     const aMW: Array<MiddlewareFunc> = allComponents
+     .filter(_ => _.type === ComponentType.MIDDLEWARE)
+     .map(_ => <MiddlewareFunc>_.payload)
+     .reduce((prev, curr) => prev.concat(curr), []);
 
-    this.aControllerDetails = allComponents
-    .filter(_ => _.type === ComponentType.CONTROLLER)
-    .map(_ => <ControllerDetails>_.payload)
-    .reduce((prev, curr) => prev.concat(curr), []);
+     this.aControllerDetails = allComponents
+     .filter(_ => _.type === ComponentType.CONTROLLER)
+     .map(_ => <ControllerDetails>_.payload)
+     .reduce((prev, curr) => prev.concat(curr), []);
 
 
-    debug('%s aContollerDetails: %o', TAG, this.aControllerDetails);
+     debug('%s aContollerDetails: %o', TAG, this.aControllerDetails);
 
-    this.aControllerDetails.map(r => {
-      r.requestMethods.map(rm => {
-        let httpMethod = RequestMethod[rm].toLowerCase();
-        debug(`Adding route ${r.name} for ${httpMethod} path: ${r.routePath}`);
-        this.router.addRoute(r.routePath, RequestMethod[rm], r.ctrl, r.name);
-        debug('added route ', r.routePath, RequestMethod[rm], r.name);
-      });
+     this.aControllerDetails.map(r => {
+     r.requestMethods.map(rm => {
+     let httpMethod = RequestMethod[rm].toLowerCase();
+     debug(`Adding route ${r.name} for ${httpMethod} path: ${r.routePath}`);
+     this.router.addRoute(r.routePath, RequestMethod[rm], r.ctrl, r.name);
+     debug('added route ', r.routePath, RequestMethod[rm], r.name);
+     });
 
-    });
+     });
 
-    aMW.push(routerWrapper(this.router));
-    aMW.push(responseWriter);
+     aMW.push(routerWrapper(this.router));
+     aMW.push(responseWriter);
 
-    this.handlerStack = aMW.sort((a, b) => {
-      return a[SYM_MIDDLEWARE_PRIORITY] - b[SYM_MIDDLEWARE_PRIORITY];
-    });
+     this.handlerStack = aMW.sort((a, b) => {
+     return a[SYM_MIDDLEWARE_PRIORITY] - b[SYM_MIDDLEWARE_PRIORITY];
+     });
 
-    this.handlerStack.map(_ => debug(`${_[SYM_MIDDLEWARE_NAME]} = ${_[SYM_MIDDLEWARE_PRIORITY]}`));
+     this.handlerStack.map(_ => debug(`${_[SYM_MIDDLEWARE_NAME]} = ${_[SYM_MIDDLEWARE_PRIORITY]}`));
 
-    registerProcessEventListeners(Container);
-    registerProcessEventListeners(this);
-*/
+     registerProcessEventListeners(Container);
+     registerProcessEventListeners(this);
+     */
 
   }
 
-/*
-  private initAllRoutesComponent(container: IContainer): Promise<IContainer> {
-    debug('entered initAllRoutesComponent');
-    const routesComponent: AllRoutes = container.getComponent(SYM_ALL_ROUTES);
-    routesComponent.allControllers = this.aControllerDetails;
+  /*
+   private initAllRoutesComponent(container: IContainer): Promise<IContainer> {
+   debug('entered initAllRoutesComponent');
+   const routesComponent: AllRoutes = container.getComponent(SYM_ALL_ROUTES);
+   routesComponent.allControllers = this.aControllerDetails;
 
-    return Promise.resolve(container);
-  }*/
+   return Promise.resolve(container);
+   }*/
 
 
   onExit(): Promise<number> {
@@ -191,20 +184,21 @@ export class Application {
       //let cleared = that.router.reset();
       //console.log(`Cleared ${cleared} router nodes`);
       resolve(1);
-    })
+    });
   }
-/*
-  private setCustomErrorHandler(c: IContainer) {
-    debug(`${TAG} Entered setCustomErrorHandler()`);
-    if (c.hasComponent(SYM_ERROR_HANDLER)) {
-      const eh = <AppErrorHandler>c.getComponent(SYM_ERROR_HANDLER);
-      debug(`${TAG} setting customErrorHandler ${eh.constructor.name}`);
-      this.customErrorHandler = ctx => Reflect.apply(eh.handleError, eh, [ctx]);
-    } else {
-      debug(`${TAG} Custom Error Handler Not Found in Container`);
-      this.customErrorHandler = this.errorHandler;
-    }
-  }*/
+
+  /*
+   private setCustomErrorHandler(c: IContainer) {
+   debug(`${TAG} Entered setCustomErrorHandler()`);
+   if (c.hasComponent(SYM_ERROR_HANDLER)) {
+   const eh = <AppErrorHandler>c.getComponent(SYM_ERROR_HANDLER);
+   debug(`${TAG} setting customErrorHandler ${eh.constructor.name}`);
+   this.customErrorHandler = ctx => Reflect.apply(eh.handleError, eh, [ctx]);
+   } else {
+   debug(`${TAG} Custom Error Handler Not Found in Container`);
+   this.customErrorHandler = this.errorHandler;
+   }
+   }*/
 
 
   /**
@@ -216,36 +210,37 @@ export class Application {
    */
   /*public handleRequest(req: http.IncomingMessage, res: http.ServerResponse) {
 
-    const ctx = new Context(req, res);
-    const handlerPromise = this.handlerStack.reduce((prev, next) => prev.then(next), Promise.resolve(<IContext>ctx));
-    const runners: Array<Promise<IContext>> = [handlerPromise];
-    if (this.timeout > 0) {
-      runners.push(rejectLater(this.timeout));
-    }
+   const ctx = new Context(req, res);
+   const handlerPromise = this.handlerStack.reduce((prev, next) => prev.then(next), Promise.resolve(<IContext>ctx));
+   const runners: Array<Promise<IContext>> = [handlerPromise];
+   if (this.timeout > 0) {
+   runners.push(rejectLater(this.timeout));
+   }
 
-    Promise.race(runners)
-    .catch(this.customErrorHandler(ctx))
-    .catch(this.errorHandler(ctx));
-    debug('handleRequest method called');
-  }*/
+   Promise.race(runners)
+   .catch(this.customErrorHandler(ctx))
+   .catch(this.errorHandler(ctx));
+   debug('handleRequest method called');
+   }*/
 
   /**
    * Important - handleRequest method must be defined before this method otherwise
    * it is not visible inside init()
    * @returns {Promise<(req:http.IncomingMessage, res:http.ServerResponse)=>undefined>}
    */
+
   /*init() {
-    return Container.init()
-    //.then(_ => this.initAllRoutesComponent(_))
-    .then(_ => {
-      this.setCustomErrorHandler(_)
-    })
-    .then(_ => {
-      return (req: http.IncomingMessage, res: http.ServerResponse) => {
-        this.handleRequest(req, res);
-      }
-    });
-  }*/
+   return Container.init()
+   //.then(_ => this.initAllRoutesComponent(_))
+   .then(_ => {
+   this.setCustomErrorHandler(_)
+   })
+   .then(_ => {
+   return (req: http.IncomingMessage, res: http.ServerResponse) => {
+   this.handleRequest(req, res);
+   }
+   });
+   }*/
 
   toString() {
     return 'Application Instance';
--- src/lib/interfaces/controller.ts
+++ src/lib/interfaces/controller.ts
@@ -1,13 +1,13 @@
 import {IAppResponse} from "./appresponse";
-import {RequestMethod} from "../enums/requestmethods";
 import Context from '../../components/context';
 export type ControllerFunc = (...args: any[]) => Promise<IAppResponse>
 export type IController = (ctx: Context) => Promise<IAppResponse>
+import HTTPMethod from 'http-method-enum';
 
 
 export interface ControllerDetails {
   name:string,
-  requestMethods:Array<RequestMethod>,
+  requestMethods:Array<HTTPMethod>,
   routePath:string,
   ctrl:IController
 }

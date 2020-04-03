2cbceb6770d7123a7d652fff0f32750881ad78f3
Dmitri <dmitri.snytkine@adp.com>
2020-04-03 18:07:42 -0400

    Added makemethoddecorator a function to create custom Controller Middleware Decorators

--- src/lib/decorators/controller/index.ts
+++ src/lib/decorators/controller/index.ts
@@ -1,5 +1,6 @@
 export * from './noargmethodparams';
 export { default as makeParamDecorator } from './makeparamdecorator';
+export { default as makeMethodDecorator } from './makemethoddecorator';
 export { default as RequestMatch } from './controllermatcher';
 export * from './validate';
 export * from './complexmethodparams';

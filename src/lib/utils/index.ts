449e559454de9536083587a989586089c771520b
Dmitri <dmitri.snytkine@adp.com>
2020-04-20 07:56:50 -0400

    Added validateResponseCode to util

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -2,3 +2,4 @@ export * from './parsebody';
 export { default as JSONPath } from './jsonpath';
 export { default as JMESPath } from './jmespath';
 export { default as isStringOrNumberOrBoolean } from './isstringnumberboolean';
+export { default as validateResponseCode } from './validateresponsecode';
